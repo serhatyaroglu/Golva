c0378ff8f59ad0f936f6c1c1425944dba3726a11
ertugrulkulaber <77529711+ertugrulkulaber@users.noreply.github.com>
2021-02-09 10:44:08 +0300

    hata düzeltme

---
+++ Golva/istatistikCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  istatistikCollectionViewCell.swift
+//  Golva
+//
+//  Created by Ertuğrul Kulaber on 9.02.2021.
+//
+
+import UIKit
+
+class istatistikCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Golva/istatistikViewController.swift
@@ -0,0 +1,30 @@
+//
+//  istatistikViewController.swift
+//  Golva
+//
+//  Created by Ertuğrul Kulaber on 9.02.2021.
+//
+
+import UIKit
+
+class istatistikViewController: UIViewController {
+
+    @IBOutlet weak var CollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
