85f596307415ff3de3e17c51568f4bbc4ebec34c
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2021-02-09 09:51:37 +0300

    turEkleVıew

---
+++ Golva/TurEkleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TurEkleCollectionViewCell.swift
+//  Golva
+//
+//  Created by serhat yaroglu on 9.02.2021.
+//
+
+import UIKit
+
+class TurEkleCollectionViewCell: UICollectionViewCell {
+    
+}
