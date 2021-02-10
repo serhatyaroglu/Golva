cbad54564e5ef2494cd0fff3682f450a7b37e373
serhatyaroglu <61588155+serhatyaroglu@users.noreply.github.com>
2021-02-10 21:20:34 +0300

    fdf

---
+++ Golva/Golva 2/istatistikCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  istatistikCollectionViewCell.swift
+//  Golva
+//
+//  Created by Ertuğrul Kulaber on 9.02.2021.
+//
+
+import UIKit
+
+class istatistikCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ziplineLabel: UILabel!
+    
+
+    @IBOutlet weak var HaftaLabelName: UILabel!
+    @IBOutlet weak var GunLabelName: UILabel!
+
+    @IBOutlet weak var gunLabel: UILabel!
+    
+    @IBOutlet weak var haftaLabel: UILabel!
+    
+
+    @IBOutlet weak var AyLabelName: UILabel!
+    
+    @IBOutlet weak var ayLabel: UILabel!
+    
+    @IBOutlet weak var resimImage: UIImageView!
+    
+   
+
+   
+    
+    
+    
+}
