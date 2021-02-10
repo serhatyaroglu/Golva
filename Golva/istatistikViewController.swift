6a7597e4a5c342f71ae4d9e5254d413483ffca60
ertugrulkulaber <77529711+ertugrulkulaber@users.noreply.github.com>
2021-02-10 09:02:27 +0300

    JJ

--- Golva/istatistikViewController.swift
+++ Golva/istatistikViewController.swift
@@ -7,24 +7,54 @@
 
 import UIKit
 
-class istatistikViewController: UIViewController {
-
-    @IBOutlet weak var CollectionView: UICollectionView!
+class istatistikViewController: UIViewController, UICollectionViewDelegate {
+   @IBOutlet weak var CollectionView: UICollectionView!
+    
+    var etkinlikListesi = [Modelleme]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        CollectionView.delegate = self
+        CollectionView.dataSource = self
+        
+        let e1 = Modelleme(etkinlikAdi: "Zipline" , gun: "5", hafta: "15" , ay: "35" , resim: "zipline")
+        let e2 = Modelleme(etkinlikAdi: "Rafting" , gun: "4", hafta: "13" , ay: "32" , resim: "rafting")
+        let e3 = Modelleme(etkinlikAdi: "Safari" , gun: "3", hafta: "17" , ay: "23" , resim: "yaylasafari")
+        let e4 = Modelleme(etkinlikAdi: "Atv" , gun: "6", hafta: "11" , ay: "34" , resim: "atvsafari")
+        
+        
+        etkinlikListesi.append(e1)
+        etkinlikListesi.append(e2)
+        etkinlikListesi.append(e3)
+        etkinlikListesi.append(e4)
+       
+        
+    }
+
+}
+extension istatistikViewController:UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return etkinlikListesi.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let etkinlik = etkinlikListesi[indexPath.row]
+        
+    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "etkinlikBilgi" , for: indexPath) as! istatistikCollectionViewCell
+        
+        cell.ziplineLabel.text = etkinlik.etkinlikAdi!
+        cell.ayLabel.text = etkinlik.ay!
+        cell.gunLabel.text = etkinlik.gun!
+        cell.haftaLabel.text = etkinlik.hafta
+        cell.resimImage.image = UIImage(named:etkinlik.resim!)
+       
+        return cell
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
